4844e63499a9999a46bf441b90725f5b82de9205
Rick <rickzhang@live.ca>
2021-01-23 13:20:50 -0500

    Add the utility to shuffle an array

--- utils/helpers.js
+++ utils/helpers.js
@@ -20,8 +20,29 @@ const playlistIdMap = {
   '37i9dQZEVXcFM6piYkUYgR': 'Viv',   // Viv
 };
 
+// Taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+const shuffle = (array) => {
+  var index = array.length, temp, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== index) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * index);
+    index -= 1;
+
+    // And swap it with the current element.
+    temp = array[index];
+    array[index] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+
+  return array;
+}
+
 module.exports = {
   getArtistsAndNames,
   playlistIdMap,
   lookUpOwner,
+  shuffle,
 }
